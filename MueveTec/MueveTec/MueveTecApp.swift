3a74ab0beb68db90164c1e583a467a45597d4962
BrunoMqzp <A00834415@tec.mx>
2025-03-28 22:35:45 -0600

    START APP

---
+++ MueveTec/MueveTec/MueveTecApp.swift
@@ -0,0 +1,17 @@
+//
+//  MueveTecApp.swift
+//  MueveTec
+//
+//  Created by Alumno on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MueveTecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
